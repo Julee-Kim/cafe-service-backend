de2c9d4dc923426c441879131b590f29f49a6d55
julee kim <summerbreeze0323@gmail.com>
2021-05-06 00:05:33 +0900

    database connect url

--- src/app.module.ts
+++ src/app.module.ts
@@ -48,11 +48,15 @@ console.log(process.env.DB_PASSWORD)
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL } // for heroku
+        : { // for local
+          host: process.env.DB_HOST,
+          port: +process.env.DB_PORT,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+        }),
       synchronize: process.env.NODE_ENV !== 'production',
       logging: true,
       entities: [Menu, User, Category, Sido, Gugun, Store, Cart, Payment],
